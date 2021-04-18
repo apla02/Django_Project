1d5aa62a7369f3a518918572f5809471f56753ef
apla02 <apla02@gmail.com>
2021-04-18 15:09:50 -0500

    adding models

--- project/poll_app/admin.py
+++ project/poll_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+"""
+option 1
+admin.site.register(Question)
+admin.site.register(Choice)
+"""
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['published_date'],'classes':['collapse'] }),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- project/poll_app/views.py
+++ project/poll_app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("<h1>Hello, Django</h1>")
+    latest_question_list = Question.objects.order_by('-published_date')[:5]
+    template = loader.get_template('poll_app/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at the question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking at the result of the question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on the question {question_id}")
+
 
 
